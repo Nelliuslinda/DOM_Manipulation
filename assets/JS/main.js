cb5360bfbe9d84616634a8110d5f8523fe5ac143
Nellius Kyalo <nelliuskyalo@gmail.com>
2022-10-26 12:00:35 +0300

    feat : add animations and sound effects on index page

---
+++ assets/JS/main.js
@@ -0,0 +1,34 @@
+//Function for playing Key script
+function playKey(e) {
+    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+
+    //Enable animation on key
+    const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
+
+    //No audio found
+    if(!audio) return;
+
+    //Rewind song
+    audio.currentTime = 0;
+
+    //Audio found
+    audio.play();
+
+    //animation
+    key.classList.add('playing');
+  }
+
+    //remove animation
+    function removeTransition(e){
+      if(e.propertyName !== 'transform'){
+        //This skips if it's not a transform
+        return;
+      }
+      this.classList.remove('playing');
+    }
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+    //Adding an event listener to the keys
+    window.addEventListener("keydown", playKey);
